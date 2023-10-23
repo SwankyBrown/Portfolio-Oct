966017153999433059d8fd401b2aca4a81be9017
Sheldon Christiansen <bigbrownguy724@gmail.com>
2023-10-23 12:22:05 -0600

    filled in blanks: working on bugs

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -18,28 +18,28 @@ function skill() {
             <img src={Frontenddevelopment} alt="Front End Development" className="skillBarImg" />
             <div className="skillBarText">
               <h2>Front End Development</h2>
-              <p>demo text</p>
+              <p>I specialize in building user interfaces and interactive web applications, bringing design to life with code. As a front-end developer, I focus on crafting engaging and responsive web experiences.</p>
             </div>
           </div>
           <div className="skillBar">
             <img src={UIdesign} alt="" className="skillBarImg" />
             <div className="skillBarText">
               <h2>UI/UX</h2>
-              <p>This is a demo text</p>
+              <p> Creating intuitive and visually appealing user interfaces that enhance the overall user experience. My role as a UI/UX designer is to make digital products both user-friendly and aesthetically pleasing.</p>
             </div>
           </div>
           <div className="skillBar">
             <img src={webDesign} alt="" className="skillBarImg" />
             <div className="skillBarText">
               <h2>Web Design</h2>
-              <p>demo text</p>
+              <p>I excel in crafting visually stunning and functional website layouts that captivate users. As a web designer, my focus is on creating engaging online experiences through aesthetics and usability.</p>
             </div>
           </div>
           <div className="skillBar">
             <img src={webDesign} alt="" className="skillBarImg" />
             <div className="skillBarText">
               <h2>Adobe Photoshop</h2>
-              <p>demo text</p>
+              <p>I'm a self-taught Adobe Photoshop enthusiast with a passion for digital image manipulation and graphic design. I've honed my skills in Photoshop to bring creativity to life through visual storytelling and editing.</p>
             </div>
           </div>
         </div>
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,7 +11,7 @@ function Intro() {
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Sheldon</span> <br /> Website Developer</span>
             <p className="introPara">I'm a web developer with solid experience in making websites that <br /> look great and are super user-friendly.</p>
-            <Link><button className="btn"><img src={btnImg} alt="Hire me" className='btnImg' />Hire Me</button></Link>
+            <Link to=''><button className="btn"><img src={btnImg} alt="Hire me" className='btnImg' />Hire Me</button></Link>
         </div>
         <img src={bg} alt="" className="bg" />
     </section>
